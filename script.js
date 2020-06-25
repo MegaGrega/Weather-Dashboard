1b94f469528ff3e6bc54a45bbcf800cf41ae8aff
MegaGrega <mkmgrega@gmail.com>
2020-06-24 23:28:30 -0700

    fixed some small edge cases + added color to uv index

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
-var locations = JSON.parse(localStorage.getItem("locations"))                                                                     // Sets array to local storage
-if(locations == null){                                                                                                            // Checks if array is null - if so sets it to empty
+var locations = JSON.parse(localStorage.getItem("locations"))                                                                                   // Sets array to local storage
+if(locations == null){                                                                                                                          // Checks if array is null - if so sets it to empty
     locations = []
 }
-createList()                                                                                                                      // Creates City list from locations array
-
+createList() 
+if(locations.length > 0){                                                                                                                       // Checks if anything is in the array
+    cityInfo(locations[locations.length-1])                                                                                                     // Loads the last value
+}
 $("#submit-button").on("click", function (event) {                                                                                              // On click of the submit button
     event.preventDefault()                                                                                                                      // Prevent Default - Prevent Refresh
     //if $("#input-field").val() is not valid ask them to put in a correct city and
@@ -15,7 +17,6 @@ $("#submit-button").on("click", function (event) {                              
         event.preventDefault()     
         cityInfo(this.getAttribute("data-name") )
     })
-
 })
 
 $(".cityItem").on("click", function (event) {                                                                                                   // On click of a City List Item
@@ -48,30 +49,39 @@ function cityInfo(searchCity) {
             url: queryForecastURL,
             method: "GET"
         }).then(function (response) {
-            var forecastHolder = $("#forecast-holder")                                                                                           // The row that holds the forecast cards
-            forecastHolder.empty();                                                                                                              // Empties out previous cards
+            var forecastHolder = $("#forecast-holder")                                                                                          // The row that holds the forecast cards
+            forecastHolder.empty();                                                                                                             // Empties out previous cards
             cityUV.text("UV Index: " + response.daily[0].uvi)
-            for (i = 1; i < 6; i++) {                                                                                                            // Loops 5 Times
+            if(response.daily[0].uvi <= 2){                                                                                                     // If statements to color UV text
+                document.getElementById("uv-index").style.backgroundColor = "green";
+            }else if (response.daily[0].uvi >2 & response.daily[0].uvi <=5){
+                document.getElementById("uv-index").style.backgroundColor = "yellow";
+            }else if (response.daily[0].uvi >5 & response.daily[0].uvi <=7){
+                document.getElementById("uv-index").style.backgroundColor = "orange";
+            }else{
+                document.getElementById("uv-index").style.backgroundColor = "red";
+            }
 
-                var iconTag = response.daily[i].weather[0].icon                                                                                  // Stores icon tag variable
-                var column = $('<div class = "col-auto"></div>')                                                                                   // Creates column variable
-                var card = $('<div class = "card"></div>')                                                                                       // Creates card variable
-                var cardBody = $('<div class = "card-body weather-card"></div>')                                                                 // creates Card Body variable
-                var iconImg = $(`<img id="wicon" src="http://openweathermap.org/img/w/${iconTag}.png"alt="Weather icon">`)                       // Creates Img Element with icontag url
+
+            for (i = 1; i < 6; i++) {                                                                                                           // Loops 5 Times
+
+                var iconTag = response.daily[i].weather[0].icon                                                                                 // Stores icon tag variable
+                var column = $('<div class = "col-auto"></div>')                                                                                // Creates column variable
+                var card = $('<div class = "card"></div>')                                                                                      // Creates card variable
+                var cardBody = $('<div class = "card-body weather-card"></div>')                                                                // creates Card Body variable
+                var iconImg = $(`<img id="wicon" src="http://openweathermap.org/img/w/${iconTag}.png"alt="Weather icon">`)                      // Creates Img Element with icontag url
                 var tempText = $(`<p>Temp: ${response.daily[i].temp.day}</p>`)
                 var humidText = $(`<p>Humidity: ${response.daily[i].humidity}</p>`)
                 var date = moment(response.daily[i].dt * 1000).format('DD-MM-YYYY')
                 cardBody.append(date, iconImg, tempText, humidText)
-                card.append(cardBody)                                                                                                             // Appends cardbody to card
-                column.append(card)                                                                                                               // Appends card to column
-                forecastHolder.append(column)                                                                                                     // Appends column to forecast row
+                card.append(cardBody)                                                                                                            // Appends cardbody to card
+                column.append(card)                                                                                                              // Appends card to column
+                forecastHolder.append(column)                                                                                                    // Appends column to forecast row
             }
 
         })
     })
-
 }
-
 function createList(){                                                                                                                             // Creates city list items
     var searchHolder = $("#saved-search-holder")                                                                                                   // Container on the page
     searchHolder.empty()
